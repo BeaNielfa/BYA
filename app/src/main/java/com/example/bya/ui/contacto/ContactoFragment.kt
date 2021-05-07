e8fbbd11ee258a4e6917d105fce4b26839352a3e
BeaNielfa <beanielfa1396@gmail.com>
2021-05-07 12:55:33 +0200

    Diseño Login y Registro

---
+++ app/src/main/java/com/example/bya/ui/contacto/ContactoFragment.kt
@@ -0,0 +1,23 @@
+package com.example.bya.ui.contacto
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bya.R
+
+class ContactoFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+
+        return root
+    }
+}
