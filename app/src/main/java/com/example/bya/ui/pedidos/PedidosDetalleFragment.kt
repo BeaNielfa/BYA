43a6d3d3d444909f3949a9e28bf49bb1f57bbbeb
BeaNielfa <beanielfa1396@gmail.com>
2021-05-31 11:03:13 +0200

    Funcionalidad gestión de pedidos listo

--- app/src/main/java/com/example/bya/ui/pedidos/PedidosDetalleFragment.kt
+++ app/src/main/java/com/example/bya/ui/pedidos/PedidosDetalleFragment.kt
@@ -1,60 +1,197 @@
 package com.example.bya.ui.pedidos
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
 import com.example.bya.R
+import com.example.bya.clases.Pedido
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_anadir_prenda.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class PedidosDetalleFragment(private val p : Pedido) : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener  {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PedidosDetalleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PedidosDetalleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var imgPedido : ImageView
+    private lateinit var tvIdCliente : TextView
+    private lateinit var tvNombre : TextView
+    private lateinit var tvTalla : TextView
+    private lateinit var tvReferencia : TextView
+    private lateinit var btnEnviar : Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    //Variables Mapa
+    private lateinit var mMap: GoogleMap
+    private var posicion: LatLng? = null
+    private var PERMISOS: Boolean = true
+
+    private val db = FirebaseFirestore.getInstance()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pedidos_detalle, container, false)
+        val root = inflater.inflate(R.layout.fragment_pedidos_detalle, container, false)
+
+        imgPedido = root.findViewById(R.id.imgDetallePedidoFoto)
+        tvIdCliente = root.findViewById(R.id.tvDetallePedidoIdCliente)
+        tvNombre = root.findViewById(R.id.tvDetallePedidoNombre)
+        tvTalla = root.findViewById(R.id.tvDetallePedidoTalla)
+        tvReferencia = root.findViewById(R.id.tvDetallePedidoReferencia)
+        val imgX : ImageView = root.findViewById(R.id.imgDetallePedidoCerrar)
+        btnEnviar = root.findViewById(R.id.btnDetallePedidoEnviar)
+
+        cargarCampos()
+        initMapa()
+
+        btnEnviar.setOnClickListener {
+
+            db.collection("pedidos").document(p.idPedido).update("estado", 1)
+            btnEnviar.isEnabled = false
+            btnEnviar.setBackgroundColor(resources.getColor(R.color.browser_actions_bg_grey))
+            Toast.makeText(requireContext(), "Pedido enviado", Toast.LENGTH_SHORT).show()
+
+        }
+
+        imgX.setOnClickListener {
+            pedidos()
+        }
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PedidosDetalleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PedidosDetalleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun cargarCampos() {
+
+        if (p.estado == 1){
+            btnEnviar.isEnabled = false
+            btnEnviar.setBackgroundColor(resources.getColor(R.color.browser_actions_bg_grey))
+        }
+
+        db.collection("prendas")
+            .whereEqualTo("idPrenda", p.idPrenda)
+            .get()
+            .addOnSuccessListener { result ->
+                for (prenda in result) {
+
+                    val nombre = prenda.get("nombre").toString()
+                    val referencia = prenda.get("referencia").toString()
+                    val foto = prenda.get("foto").toString()
+
+                    tvIdCliente.text = p.idUsuario
+                    tvReferencia.text = referencia
+                    tvNombre.text = nombre
+                    tvTalla.text = p.talla
+                    Picasso.get().load(Uri.parse(foto)).into(imgPedido)
+
                 }
             }
+
+
     }
+
+
+    //Inicialiazamos el mapa
+    private fun initMapa() {
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.mapaDetallePedidoMapa) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        configurarIUMapa()
+        cargarMapa()
+    }
+
+    /**
+     * Configuración del mapa
+     */
+    private fun configurarIUMapa() {
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        val uiSettings: UiSettings = mMap.uiSettings
+        //gestos
+        uiSettings.isScrollGesturesEnabled = true
+        uiSettings.isTiltGesturesEnabled = true
+        //brújula
+        uiSettings.isCompassEnabled = true
+        //zoom
+        uiSettings.isZoomControlsEnabled = true
+        //barra de herramientas
+        uiSettings.isMapToolbarEnabled = true
+        //zoom por defecto
+        mMap.setMinZoomPreference(14.0f)
+        mMap.setOnMarkerClickListener(this)
+    }
+
+
+    /**
+     * Cargamos el mapa
+     */
+    @SuppressLint("MissingPermission")
+    private fun cargarMapa() {
+        if (this.PERMISOS) {
+            mMap.isMyLocationEnabled = true
+        }
+        //activarEventosMarcadores()
+        obtenerPosicion()
+    }
+
+
+
+    /**
+     * Obtiene la posición actual para pasarsela al mapa y que se cargue en nuestra posición
+     */
+    private fun obtenerPosicion() {
+
+        posicion = LatLng(
+            p.latitud.toDouble(),
+            p.longitud.toDouble()
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(posicion));
+
+        mMap.addMarker(
+            MarkerOptions() // Posición
+                .position(posicion) // Título
+                .title("Posición Pedido") // título
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+        )
+
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        return false
+    }
+
+    private fun pedidos(){
+
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        transaction.add(R.id.detallePedido, PedidosFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/bya/ui/pedidos/PedidosFragment.kt
+++ app/src/main/java/com/example/bya/ui/pedidos/PedidosFragment.kt
@@ -1,22 +1,91 @@
 package com.example.bya.ui.pedidos
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bya.R
+import com.example.bya.clases.Pedido
+import com.example.bya.ui.catalogoUsuario.CatalogoUsuarioDetalleFragment
+import com.example.bya.ui.historial.HistorialListAdapter
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class PedidosFragment : Fragment() {
+
+    private lateinit var recy : RecyclerView
+    private var listaPedidos = mutableListOf<Pedido>() //Lista de favoritos
+    private lateinit var pedidosAdapter: PedidosListAdapter
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pedidos, container, false)
+        val root = inflater.inflate(R.layout.fragment_pedidos, container, false)
+
+        //detecta cuando pulsamos en un item
+        pedidosAdapter = PedidosListAdapter(requireContext(), listaPedidos) {
+            eventoClicFila(it)
+        }
+
+        recy = root.findViewById(R.id.pedidoRecycler)
+
+        recy.layoutManager = LinearLayoutManager(context)
+
+        rellenarArrayPedidos()
+
+        return root
     }
 
+    private fun rellenarArrayPedidos() {
+
+        db.collection("pedidos")
+            .addSnapshotListener{ snapshot, e->
+                listaPedidos.clear()
+                for (historial in snapshot!!) {
+
+                    val idPedido = historial.get("idPedido").toString()
+                    val idPrenda = historial.get("idPrenda").toString()
+                    val idUsuario = historial.get("idUsuario").toString()
+                    val fechaCompra = historial.get("fechaCompra").toString()
+                    val latitud = historial.get("latitud").toString()
+                    val longitud = historial.get("longitud").toString()
+                    val talla = historial.get("talla").toString()
+                    val estado = historial.get("estado").toString().toInt()
+
+                    val p = Pedido(idPedido, idPrenda, idUsuario, fechaCompra, latitud, longitud, talla, estado)
+
+                    listaPedidos.add(p)
+                }
+
+                recy.adapter = pedidosAdapter
+
+            }
+
+    }
+
+    private fun eventoClicFila(pedido: Pedido) {
+        abrirPedido(pedido)
+    }
+
+    private fun abrirPedido(pedido: Pedido) {
+
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        transaction.add(R.id.fragmentPedidos, PedidosDetalleFragment(pedido))
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
 
 }
